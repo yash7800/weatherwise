0074291b20f2c3ab1d0076bd69674d92b120f611
Yash Sharma <130568467+yash7800@users.noreply.github.com>
2024-04-13 14:45:10 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ searchBox.addEventListener('input', function() {
 
   if (userInput.length > 2) {
     // Make an API request to fetch city suggestions based on userInput
-    fetch(`https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/geonames-all-cities-with-a-population-1000/records?limit=20?query=${userInput}`)
+    fetch(`https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/geonames-all-cities-with-a-population-1000/records?limit=20&refine=cou_name_en%3A%22Afghanistan%22&refine=cou_name_en%3A%22Angola%22&refine=cou_name_en%3A%22Algeria%22&refine=cou_name_en%3A%22Costa%20Rica%22&refine=cou_name_en%3A%22Congo%2C%20Democratic%20Republic%20of%20the%22&refine=cou_name_en%3A%22Central%20African%20Republic%22&refine=cou_name_en%3A%22Cameroon%22&refine=cou_name_en%3A%22Burundi%22&refine=cou_name_en%3A%22Burkina%20Faso%22&refine=cou_name_en%3A%22Brunei%20Darussalam%22&refine=cou_name_en%3A%22Brazil%22&refine=cou_name_en%3A%22India%22&refine=cou_name_en%3A%22Iceland%22&refine=cou_name_en%3A%22Indonesia%22&refine=cou_name_en%3A%22Iran%2C%20Islamic%20Rep.%20of%22&refine=cou_name_en%3A%22Iraq%22&refine=cou_name_en%3A%22Ireland%22&refine=cou_name_en%3A%22Isle%20of%20Man%22&refine=cou_name_en%3A%22Italy%22&refine=cou_name_en%3A%22Japan%22&refine=cou_name_en%3A%22Jordan%22&refine=cou_name_en%3A%22Kyrgyzstan%22?query=${userInput}`)
       .then(response => response.json())
       .then(data => {
         data.forEach(city => {
